a93de1cca3d3163603253997635673da7f59762e
Hamptooon <karachevigor21@gmail.com>
2025-04-01 20:27:37 +0300

    config: config project for github-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todos-intern-task/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
